398232348a301b8521cc30dde544174cbc4d193a
Newton <ofosunewton47@gmail.com>
2026-08-14 16:58:14 +0000

    Implement ship sinking logic

--- src/Test/ship.test.js
+++ src/Test/ship.test.js
@@ -1,20 +1,39 @@
 import { Ship } from "../ship_class.js"
 //test for a newly(ship 1) created ship with 0 hits
-test('newly created destroyer should return 0', () => {
-    let destroyer = new Ship(1)
-    expect(destroyer.hit()).toBe(1);
-    expect(destroyer.hit()).toBe(2);
-    expect(destroyer.hit()).toBe(3);
+// test('newly created destroyer should return 0', () => {
+//     let destroyer = new Ship(1)
+//     expect(destroyer.hit()).toBe(1);
+//     expect(destroyer.hit()).toBe(2);
+//     expect(destroyer.hit()).toBe(3);
 
-})
+// })
 
 //ship 2
-test('next hit should change this.hits to 2', () => {
-    let destroyer = new Ship(2)
-    expect(destroyer.hit()).toBe(1)
-    expect(destroyer.hit()).toBe(2)
-    expect(destroyer.hit()).toBe(3)
+// test('next hit should change this.hits to 2', () => {
+//     let destroyer = new Ship(2)
+//     expect(destroyer.hit()).toBe(1)
+//     expect(destroyer.hit()).toBe(2)
+//     expect(destroyer.hit()).toBe(3)
 
+// })
+
+//test for isSunk
+test('check if a ship has enough hit to sink', () => {
+    let destroyer = new Ship(2);
+    expect(destroyer.hit()).toBe(1)
+    expect(destroyer.isSunk()).toBe(false);
 })
+
+//test for isSunk ship 2
+test('check if a ship has enough hit to sink(ship 2)', () => {
+    let destroyer = new Ship(2);
+    expect(destroyer.hit()).toBe(1);
+    expect(destroyer.hit()).toBe(2);
+    expect(destroyer.isSunk()).toBe(true);
+})
+
+
+
+
 
 
--- src/ship_class.js
+++ src/ship_class.js
@@ -2,7 +2,7 @@ export class Ship {
     constructor(length) {
         this.length = length,
         this.hits = 0
-        this.sunk = false;
+        this.sunk;
         
     } 
 
@@ -10,6 +10,14 @@ export class Ship {
        let current_hit_record = this.hits + 1;
        this.hits = current_hit_record;
        return this.hits;
+    }
+
+    isSunk() {
+        if (this.hits >= this.length) {
+            return true
+        } else {
+            return false
+        }
     }
 }
 
